e6d04a1d70fb7c0aaa0d0886bef4158525bce3a2
joesanthosh24 <joe.santhosh88@yahoo.com>
2023-06-05 20:17:41 -0400

    Added routing configuration and basic home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import './App.css';
+import "./App.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import Navbar from './components/navbar/navbar.component';
+import Navbar from "./components/navbar/navbar.component";
+import HomePage from "./pages/home/home-page.component";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />
+  }
+])
 
 function App() {
   return (
-    <div className='app'>
+    <div className="app">
       <Navbar />
+      <RouterProvider router={router} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
