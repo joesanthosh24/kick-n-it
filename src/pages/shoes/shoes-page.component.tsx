388f90c145ad142420335a48f5c3e285c1baa4bd
joesanthosh24 <joe.santhosh88@yahoo.com>
2023-06-20 19:39:11 -0400

    Added a card component inside shoes page

--- src/pages/shoes/shoes-page.component.tsx
+++ src/pages/shoes/shoes-page.component.tsx
@@ -1,6 +1,16 @@
 import { useParams } from "react-router-dom";
 
+import ShoeImage from '../../assets/images/shoe_image.webp';
+
 import {
+  Card,
+    CardContent,
+    CardDescription,
+    CardImage,
+    CardTitle,
+    ColorCircle,
+    ColorCircleContainer,
+    ColorSelection,
     FilterText,
   Filters,
   ProductsContainer,
@@ -24,7 +34,26 @@ const ShoesPage = () => {
         />
         <Filter filterName="Color" filterType="checkbox" />
       </Filters>
-      <ProductsContainer></ProductsContainer>
+      <ProductsContainer>
+        <Card>
+          <div>
+            <CardImage src={ShoeImage} />
+          </div>
+          <CardContent>
+            <CardTitle>Jordan 6 rings</CardTitle>
+            <CardDescription>Men's gym Red/White/Black</CardDescription>
+          </CardContent>
+          <ColorSelection>
+            <p>Colour: </p>
+            <ColorCircleContainer>
+              <ColorCircle backgroundColor="black"></ColorCircle>
+              <ColorCircle backgroundColor="gray"></ColorCircle>
+              <ColorCircle backgroundColor="red"></ColorCircle>
+              <ColorCircle backgroundColor="blue"></ColorCircle>
+            </ColorCircleContainer>
+          </ColorSelection>
+        </Card>
+      </ProductsContainer>
     </ShoesPageContainer>
   );
 };
--- src/pages/shoes/shoes-page.styles.ts
+++ src/pages/shoes/shoes-page.styles.ts
@@ -22,6 +22,61 @@ export const Filters = styled.div`
 
 export const ProductsContainer = styled.div`
   flex: 5;
-  background-color: #fff;
+  background-color: transparent;
   margin-left: 5px;
+`;
+
+export const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: lpx solid #ccc;
+  border-radius: 15px;
+  overflow: hidden;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  background-color: #fff;
+  max-width: 240px;
+  margin: 10px;
+  font-family: 'Roboto', sans-serif;
+  padding: 18px;
+`;
+
+export const CardImage = styled.img`
+  width: 70%;
+  object-fit: scale-down;
+`;
+
+export const CardContent = styled.div`
+  text-align: left;
+`;
+
+export const CardTitle = styled.h1`
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 2px;
+`;
+
+export const CardDescription = styled.p`
+  font-size: 10px;
+  margin-bottom: 0;
+`;
+
+export const ColorSelection = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 12px;
+  margin: 15px 0;
+`;
+
+export const ColorCircleContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const ColorCircle = styled.div`
+  height: 12px;
+  width: 12px;
+  background-color: ${props => props.backgroundColor ? props.backgroundColor : 'gray'};
+  border-radius: 50%;
+  margin-left: 5px;
+  cursor: pointer;
 `;
